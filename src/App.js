29f936cc9a37a50c93ea2ddf41d793000a3834fe
Charlene-Lewis8 <charmarlew08@gmail.com>
2026-04-10 00:17:08 -0400

    Dynamic routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import Nav from './components/Nav';
-import Landing from './components/Landing';
-import Highlights from './components/Highlights';
-import Featured from './components/Featured'
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Books from "./pages/Books";
+import {books} from './data';
+import BookInfo from './pages/BookInfo';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
+      <Route  path="/" exact component={Home} />
+      <Route path="/books" render={() => <Books books={books} />} />
+      <Route path="/books/1" render={() => <BookInfo books={books} />} />     
+      <Footer />
     </div>
+    </Router>
   );
 }
 
