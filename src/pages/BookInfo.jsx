414f24f6d170840f9f9c702cf1ec0e89ddf23659
Charlene-Lewis8 <charmarlew08@gmail.com>
2026-04-12 00:51:01 -0400

    cart section complete

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react'
+import React, {useState}from 'react';
 import { Link, useParams } from 'react-router-dom';
 import Rating from '../components/ui/Rating';
 import Price from '../components/ui/Price';
@@ -7,10 +7,19 @@ import Book from '../components/ui/book';
 
 
 
-const BookInfo = ({ books }) => {
+const BookInfo = ({ books, addToCart, cart }) => {
    const {id} =  useParams();
    const book = books.find(book => +book.id === +id);
-    console.log(book)
+
+
+    function addBookToCart(book) {
+        addToCart(book);
+    }
+
+    function bookExistsOnCart() {
+        return cart.find(book => +book.id === +id);
+    }
+
     return (
         <div id="books__body">
             <main id="books__main">
@@ -47,7 +56,14 @@ const BookInfo = ({ books }) => {
                                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem veniam fugit iste esse expedita atque neque natus deleniti? Quis vitae autem est fugit cum odit fuga ad nemo in culpa.
                                     </p>
                                 </div>
-                                <button className="btn">Add to Cart</button>
+                                {
+                                bookExistsOnCart() ? (
+                                <Link to="/cart" className="book__link">
+                                <button className="btn">Checkout</button>
+                                </Link>
+                                ):(
+                                <button className="btn" onClick={() => addBookToCart(book)}>Add to Cart</button>)
+                                }
                             </div>
                         </div>
                     </div>
